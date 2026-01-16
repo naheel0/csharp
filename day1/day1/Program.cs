ed15c6e129e5a501dfbf502ebb1bb1112138c866
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-01-16 12:10:50 +0530

    commit day 1 task

---
+++ day1/day1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter the first number:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Addition:" + (num1 + num2));
+Console.WriteLine("Subtraction:" + (num1 - num2));
+Console.WriteLine("Multiplication:" + (num1 * num2));
+if (num1 != 0)
+{
+    Console.WriteLine("Division: " + ( num1/num2));
+}
+else
+{
+    Console.WriteLine("Division: Cannot divide by zero");
+}
