04022ab5a580941b9bb4cd8b2341f4d58f85e418
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-01-16 15:10:29 +0530

    commit day 2 task

---
+++ day2/Task2/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter the number of row ");
+int num = int.Parse(Console.ReadLine());
+int mid = num / 2;
+for(int i = 0; i <= mid; i++)
+{
+    for(int j = 0; j < mid - i; j++) 
+    {
+        Console.Write(" ");
+
+    }
+    for(int j = 0; j < 2 * i + 1; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+for(int i = mid-1; i>=0; i--)
+{
+    for (int j = 0; j < mid - i; j++) 
+    {
+        Console.Write(" ");
+
+    }
+    for (int j = 0; j < 2 * i + 1; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
---
+++ day2/task1/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter a number:");
+int number = int.Parse(Console.ReadLine());
+bool isPrime = true;
+if (number <= 1)
+{
+    isPrime = false;
+}
+else
+{
+    for(int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+}
+if (isPrime)
+{
+    Console.WriteLine("the number is prime");
+}
+else
+{
+    Console.WriteLine("the number is not prime");
+}
