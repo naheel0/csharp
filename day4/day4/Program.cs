17335108e474aff6ef99d694e848cae02afdfe25
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-01-20 14:55:57 +0530

    commit day 4

---
+++ day4/day4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Drawing;
+
+public interface Shape
+{
+    double GetArea();
+}
+ class Cricle:Shape
+{
+    double Radius;
+    public Cricle( double radius)
+    {
+        Radius = radius;
+    }
+    public double GetArea()
+    {
+        return 3.14 * Radius * Radius;
+    }
+}
+class Rectangle : Shape
+{
+    public double Length, Width;
+    public Rectangle( double length,double width)
+    {
+        Width = width;
+        Length = length;
+    }
+    public double GetArea()
+    {
+        return Length * Width;
+    }
+}
+class Square : Shape
+{
+    public double Length;
+    public Square(double length)
+    {
+        Length = length;
+    }
+     public double GetArea()
+    {
+       return Length * Length;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Cricle c1 = new Cricle(2.5);
+        Rectangle r1 = new Rectangle(4, 3);
+        Square s1 = new Square(5);
+
+        Console.WriteLine("Circle Area: " + c1.GetArea());
+        Console.WriteLine("Rectangle Area: " + r1.GetArea());
+        Console.WriteLine("Square Area: " + s1.GetArea());
+
+    }
+}
